4efd4e5f5f80db3c5d7c301f1e90dac28429e97e
lucianoBrd <lucien.burdet@gmail.com>
2019-10-29 19:08:11 +0100

    explain project

--- src/validateur.c
+++ src/validateur.c
@@ -4,6 +4,24 @@
 
 #include "validateur.h"
 
+/* @brief
+ * Vérifie le contenu d'un message.
+ * Si le message contient le bon nombre et format de donnée
+ * en fonction de son code. Comme par exemple si le code vaut 
+ * calcule, le message doit contenir 3 valeurs dont la première 
+ * au format string et les deux autres doivent être des nombres
+ * corrects.
+ * Il est préférable d'éxécuter validateur_format_message_json
+ * avant cette fonction car cette fonction ne vérifie pas la forme
+ * globale du message (nombre paire de guillemets...).
+ * Cette fonction suppose donc que le format global du message est correct.
+ *
+ * @params
+ * message : Message JSON à vérifier.
+ *
+ * @return
+ * 0 si tout va bien ou -1 en cas d'erreur.
+ */
 int validateur_content_message_json(
   char *message
 ){
@@ -74,8 +92,24 @@ int validateur_content_message_json(
   
   delete_message_json(json);
   return 0;
-}
+  
+} /* validateur_content_message_json */
 
+/* @brief
+ * Vérifie le format global d'un message.
+ * Vérifie si le message commence bien par { et se termine
+ * par }... En somme, cette fonction vérifie que le message
+ * répond aux norme global de format.
+ * Il est préférable d'éxécuter cette fonction avant
+ * validateur_content_message_json car cette fonction vérifie la forme
+ * globale du message (nombre paire de guillemets...).
+ *
+ * @params
+ * message : Message JSON à vérifier.
+ *
+ * @return
+ * 0 si tout va bien ou -1 en cas d'erreur.
+ */
 int validateur_format_message_json(
   char *message
 ){
@@ -164,8 +198,17 @@ int validateur_format_message_json(
     
   return 0;
   
-}
+} /* validateur_format_message_json */
 
+/* @brief
+ * Vérifie qu'il y a un nombre paire de guillemets.
+ *
+ * @params
+ * message : Message JSON à vérifier.
+ *
+ * @return
+ * Position après ce que l'on vérifie si tout va bien ou -1 en cas d'erreur.
+ */
 int validateur_guille_format_message_json(
   char *message
 ){
@@ -186,8 +229,19 @@ int validateur_guille_format_message_json(
   }
   return i;
   
-}
+} /* validateur_guille_format_message_json */
 
+/* @brief
+ * Vérifie que le message commence par
+ * une accolade et se termine par une 
+ * accolade.
+ *
+ * @params
+ * message : Message JSON à vérifier.
+ *
+ * @return
+ * Position après ce que l'on vérifie si tout va bien ou -1 en cas d'erreur.
+ */
 int validateur_accolades_format_message_json(
   char *message
 ){
@@ -214,8 +268,18 @@ int validateur_accolades_format_message_json(
   }
   return i;
   
-}
+} /* validateur_accolades_format_message_json */
 
+/* @brief
+ * Vérifie que le message contient "code" au bon emplacement.
+ *
+ * @params
+ * message : Message JSON à vérifier.
+ * i : Position de démarage.
+ *
+ * @return
+ * Position après ce que l'on vérifie si tout va bien ou -1 en cas d'erreur.
+ */
 int validateur_code_format_message_json(
   char 	*message,
   int	i
@@ -246,8 +310,19 @@ int validateur_code_format_message_json(
   i = i + 2;
   return i;
   
-}
+} /* validateur_code_format_message_json */
 
+/* @brief
+ * Vérifie que le message contient un charactere au bon emplacement.
+ *
+ * @params
+ * message : Message JSON à vérifier.
+ * i : Position de démarage.
+ * char_test : Charactere à vérifier.
+ *
+ * @return
+ * Position après ce que l'on vérifie si tout va bien ou -1 en cas d'erreur.
+ */
 int validateur_char_format_message_json(
   char 	*message,
   int	i,
@@ -276,8 +351,18 @@ int validateur_char_format_message_json(
   i++;
   return i;
   
-}
+} /* validateur_char_format_message_json */
 
+/* @brief
+ * Vérifie que le message contient "valeurs" au bon emplacement.
+ *
+ * @params
+ * message : Message JSON à vérifier.
+ * i : Position de démarage.
+ *
+ * @return
+ * Position après ce que l'on vérifie si tout va bien ou -1 en cas d'erreur.
+ */
 int validateur_valeurs_format_message_json(
   char 	*message,
   int	i
@@ -311,8 +396,20 @@ int validateur_valeurs_format_message_json(
   i = i + 2;
   return i;
   
-}
+} /* validateur_valeurs_format_message_json */
 
+/* @brief
+ * Vérifie que le message contient un charactere 
+ * juste après i (ne prend pas en compte les espaces, retour à la ligne...).
+ *
+ * @params
+ * message : Message JSON à vérifier.
+ * i : Position de démarage.
+ * char_test : Charactere à vérifier.
+ *
+ * @return
+ * Position après ce que l'on vérifie si tout va bien ou -1 en cas d'erreur.
+ */
 int validateur_char_after_format_message_json(
   char 	*message,
   int	i,
@@ -335,8 +432,19 @@ int validateur_char_after_format_message_json(
   i++;
   return i;
   
-}
+} /* validateur_char_after_format_message_json */
 
+/* @brief
+ * Vérifie que le message contient un string comme valeur.
+ * Utile pour vérifier un message dont le code vaut nom ou 
+ * message.
+ *
+ * @params
+ * message : Message JSON à vérifier.
+ *
+ * @return
+ * 0 si tout va bien ou -1 en cas d'erreur.
+ */
 int validateur_string_content_message_json(
   char 	*message
 ){
@@ -385,8 +493,19 @@ int validateur_string_content_message_json(
     
     return 0;
   
-}
+} /* validateur_string_content_message_json */
 
+/* @brief
+ * Vérifie que le message contient un string comme première valeur
+ * puis deux nombres.
+ * Utile pour vérifier un message dont le code vaut calcule.
+ *
+ * @params
+ * message : Message JSON à vérifier.
+ *
+ * @return
+ * 0 si tout va bien ou -1 en cas d'erreur.
+ */
 int validateur_calcule_content_message_json(
   char 	*message
 ){
@@ -469,8 +588,19 @@ int validateur_calcule_content_message_json(
     
     return 0;
   
-}
+} /* validateur_calcule_content_message_json */
 
+/* @brief
+ * Vérifie que le message contient un nombre comme première valeur
+ * puis un string pour toutes les autres valeurs.
+ * Utile pour vérifier un message dont le code vaut couleurs.
+ *
+ * @params
+ * message : Message JSON à vérifier.
+ *
+ * @return
+ * 0 si tout va bien ou -1 en cas d'erreur.
+ */
 int validateur_couleurs_content_message_json(
   char 	*message
 ){
@@ -536,4 +666,4 @@ int validateur_couleurs_content_message_json(
     
     return 0;
   
-}
+} /* validateur_couleurs_content_message_json */
